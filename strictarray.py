27dfbf45ac5fcd878bfbc6257330ed3f9facda4a
Jun Kimura <jksmphone@gmail.com>
2012-02-03 21:53:27 +0900

    不要なコメントを削除

--- strictarray.py
+++ strictarray.py
@@ -1,16 +1,8 @@
 # -*- coding: utf-8 -*-
 
-'''
-@summary: 
-    staticな型のList
-    c/apiで実装したい　型を制限することで高速化とメモリ節減を図りたい
-'''
 
 class StrictArray(list):
-    '''
-    @todo: decoratorにしてもいいかも
-    @param: otype: <type>
-    '''
+
     def __init__(self, otype):
         if not isinstance(otype, type):
             otype = type(otype)
@@ -26,17 +18,16 @@ class StrictArray(list):
     
     def _is_type(self, value):
         ''' decoratorにしてもいいかもしれない'''
-        print "hook"
         if self.otype is not type(value):
-            print self.otype
-            print type(value)
+            #print self.otype
+            #print type(value)
             raise TypeError("Value '%s' is not match %s." % (str(value), self.otype))
         
 def test():
     s = StrictArray(str)
-    s.append("jun")
-    s.append("kimura")
-    s.append(u"noriko")
+    s.append("one")
+    s.append("two")
+    #s.append(u"three") # Error
     
 if __name__ == '__main__':
     test()
